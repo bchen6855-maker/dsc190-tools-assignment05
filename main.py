1d477798bbcd84f0845e719172a6fa1a20f3f6c5
Bill Chen <bchen6855@gmail.com>
2026-05-05 17:36:27 -0700

    fixed ruff errors in main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import numpy as np
-
 def main():
     print("Hello from dsc190-tools-assignment05!")
 
 if __name__ == "__main__":
     main()
-
-I = input()
-
-if I == None:
-    print('x is None')
